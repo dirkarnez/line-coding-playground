28ad2dc2f45a6fea3bbacc1fe0973f85c36b542f
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-25 16:55:57 +0800

    - update project/src/chip.v

--- project/src/chip.v
+++ project/src/chip.v
@@ -8,3 +8,20 @@ module chip(input clk, output led);
     assign led = counter[7];
 
 endmodule
+
+// module nrzi_encoder (
+//     input clk,
+//     input rst,
+//     input data_in,
+//     output reg nrzi_out
+// );
+//     // NRZI Encoding: 0 -> invert, 1 -> stay same
+//     always @(posedge clk or posedge rst) begin
+//         if (rst)
+//             nrzi_out <= 1'b1; // Initial state
+//         else if (data_in == 1'b0)
+//             nrzi_out <= ~nrzi_out; // Invert on 0
+//         else
+//             nrzi_out <= nrzi_out; // Stay same on 1
+//     end
+// endmodule
